d5ab88c97d11971a92e69f1a6025fa28029a0936
Dasaolu Daniel <yungdansa@gmail.com>
2025-07-11 13:35:39 +0100

    ..

--- src/components/AppNotification/index.vue
+++ src/components/AppNotification/index.vue
@@ -2,14 +2,22 @@
   <div
     class="w-full flex flex-row space-x-3 justify-between py-3 !pt-2 border-b-[1.5px] border-[#E0E2E4] mb-3"
   >
-    <div class="flex flex-row items-start space-x-2">
+    <div class="flex flex-row items-start">
       <div class="w-[50px]">
-        <app-icon :name="notification.logo" customClass="!h-[48px]" />
+        <app-icon :name="notification.icon" customClass="!h-[48px]" />
       </div>
-      <div class="w-full flex flex-col space-y-[2px] justify-start">
-        <app-normal-text class="!text-black !font-[500] text-left !text-sm">
-          {{ notification.title }}
-        </app-normal-text>
+
+      <div class="w-full flex-1 flex flex-col space-y-[3px] ml-2 justify-start">
+        <div class="flex items-center justify-between">
+          <app-normal-text class="!text-black !font-[500] text-left !text-sm">
+            {{ notification.title }}
+          </app-normal-text>
+          <app-normal-text
+            class="!text-gray-two !font-medium !text-right !whitespace-nowrap"
+          >
+            {{ notification.date }}
+          </app-normal-text>
+        </div>
         <div class="flex flex-row items-center">
           <template
             v-for="(content, index) in notification.contents"
@@ -26,45 +34,45 @@
         </div>
       </div>
     </div>
-    <div class="flex flex-row justify-end">
+    <!-- <div class="flex flex-row justify-end">
       <app-normal-text class="!text-gray-two !text-right !whitespace-nowrap">
         {{ notification.date }}
       </app-normal-text>
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import AppIcon from "../AppIcon";
-import { AppNormalText } from "../AppTypography";
+  import { defineComponent } from "vue"
+  import AppIcon from "../AppIcon"
+  import { AppNormalText } from "../AppTypography"
 
-/**
- *  Displays a transaction item with its icon, description, date, time, and amount.
- */
-export default defineComponent({
-  components: {
-    AppIcon,
-    AppNormalText,
-  },
-  name: "AppNotification",
-  props: {
-    /**
-     * The notification data.
-     */
-    notification: {
-      type: Object as () => {
-        logo: string;
-        title: string;
-        contents: string[];
-        date: string;
-      },
-      required: true,
+  /**
+   *  Displays a transaction item with its icon, description, date, time, and amount.
+   */
+  export default defineComponent({
+    components: {
+      AppIcon,
+      AppNormalText,
     },
-  },
+    name: "AppNotification",
+    props: {
+      /**
+       * The notification data.
+       */
+      notification: {
+        type: Object as () => {
+          icon: string
+          title: string
+          contents: string[]
+          date: string
+        },
+        required: true,
+      },
+    },
 
-  setup() {
-    return {};
-  },
-});
+    setup() {
+      return {}
+    },
+  })
 </script>
