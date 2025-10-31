9ea6a96ab5778352edc537d72dba6df4b10aeecb
ArchyScript <yungdansa@gmail.com>
2025-10-31 06:20:03 +0100

    switched adding poroduct to cart from categoryt to by business

--- src/components/AppCard/Product.vue
+++ src/components/AppCard/Product.vue
@@ -54,7 +54,7 @@
     >
       <div class="w-full flex flex-col items-center border-b-divider">
         <div
-          class="px-4 pb-4 w-full flex flex-col space-y-2 !z-[0] border-b-divider"
+          class="px-4 pb-3 w-full flex flex-col space-y-2 !z-[0] border-b-divider"
         >
           <app-swiper
             :free-mode="false"
@@ -74,7 +74,7 @@
             >
               <app-image-loader
                 :photoUrl="image.url"
-                class="rounded-[12px] h-[180px] w-full border-[1px] border-gray-200 dark:!border-gray-700 cursor-pointer"
+                class="rounded-[12px] h-[200px] w-full border-[1px] border-gray-200 dark:!border-gray-700 cursor-pointer"
               />
             </swiper-slide>
           </app-swiper>
@@ -121,7 +121,7 @@
         </div>
 
         <!-- Description -->
-        <div class="w-full flex flex-col pt-2 !pb-3">
+        <div class="w-full flex flex-col py-3">
           <app-normal-text class="!text-sm !font-semibold !text-left mb-1 px-4">
             Description
           </app-normal-text>
@@ -176,122 +176,122 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, ref, watch } from "vue";
-import AppImageLoader from "../AppImageLoader";
-import AppIcon from "../AppIcon";
-import AppButton from "../AppButton";
-import AppModal from "../AppModal";
-import AppAvatar from "../AppAvatar";
-import { AppNormalText } from "../AppTypography";
-import AppSwiper from "../AppSwiper";
-import { SwiperSlide } from "swiper/vue";
-import { Logic } from "../../composable";
-import { getBottomPadding } from "../../composable";
+  import { defineComponent, PropType, ref, watch } from "vue"
+  import AppImageLoader from "../AppImageLoader"
+  import AppIcon from "../AppIcon"
+  import AppButton from "../AppButton"
+  import AppModal from "../AppModal"
+  import AppAvatar from "../AppAvatar"
+  import { AppNormalText } from "../AppTypography"
+  import AppSwiper from "../AppSwiper"
+  import { SwiperSlide } from "swiper/vue"
+  import { Logic } from "../../composable"
+  import { getBottomPadding } from "../../composable"
 
-/**
- * Merchant Product Card
- */
-type ProductSource = "market" | "event" | "ticket" | "other";
-type ProductCategory = "physical" | "ticket" | "event" | "other";
+  /**
+   * Merchant Product Card
+   */
+  type ProductSource = "market" | "event" | "ticket" | "other"
+  type ProductCategory = "physical" | "ticket" | "event" | "other"
 
-interface MerchantProduct {
-  id: string | number;
-  uuid?: string;
-  name: string;
-  description: string;
-  price: number;
-  formattedPrice: string;
-  currency?: string;
-  currencySymbol?: string;
-  imageUrl: string;
-  quantity: number;
-  category: ProductCategory;
-  productType?: ProductSource;
-  selected?: boolean;
-  sku: string;
-  variant?: any;
-  images: { url: string }[];
-  meta?: Record<string, any>;
-}
+  interface MerchantProduct {
+    id: string | number
+    uuid?: string
+    name: string
+    description: string
+    price: number
+    formattedPrice: string
+    currency?: string
+    currencySymbol?: string
+    imageUrl: string
+    quantity: number
+    category: ProductCategory
+    productType?: ProductSource
+    selected?: boolean
+    sku: string
+    variant?: any
+    images: { url: string }[]
+    meta?: Record<string, any>
+  }
 
-export default defineComponent({
-  name: "AppMerchantProduct",
-  components: {
-    AppNormalText,
-    AppImageLoader,
-    AppIcon,
-    AppButton,
-    AppModal,
-    SwiperSlide,
-    AppSwiper,
-    AppAvatar,
-  },
-  props: {
-    product: {
-      type: Object as PropType<MerchantProduct>,
-      required: true,
+  export default defineComponent({
+    name: "AppMerchantProduct",
+    components: {
+      AppNormalText,
+      AppImageLoader,
+      AppIcon,
+      AppButton,
+      AppModal,
+      SwiperSlide,
+      AppSwiper,
+      AppAvatar,
     },
-    imageSize: {
-      type: Number,
-      default: 44,
+    props: {
+      product: {
+        type: Object as PropType<MerchantProduct>,
+        required: true,
+      },
+      imageSize: {
+        type: Number,
+        default: 44,
+      },
+      customClass: {
+        type: String,
+        default: "",
+      },
+      isInCart: {
+        type: Boolean,
+        default: false,
+      },
+      allowViewMerchantDetails: {
+        type: Boolean,
+        default: true,
+      },
     },
-    customClass: {
-      type: String,
-      default: "",
+    emits: ["view-product", "add-to-cart"],
+    setup(props, { emit }) {
+      const defaultBanner = "/images/greep-transparent-logo.svg"
+      const showProductDetailsModal = ref(false)
+      const seeMoreDescription = ref(false)
+      const currentSlidePosition = ref(0)
+      const slidePosition = ref(0)
+
+      const viewProductDetails = () => {
+        showProductDetailsModal.value = true
+      }
+
+      const viewProduct = () => {
+        showProductDetailsModal.value = false
+        emit("view-product", props.product)
+      }
+      const viewMerchantDetails = () => {
+        if (!props.allowViewMerchantDetails) return
+
+        showProductDetailsModal.value = false
+        Logic.Common.GoToRoute(`/shops/${props.product.businessUuid}`)
+      }
+      const toggleCart = () => {
+        if (props.isInCart) emit("remove-from-cart", props.product)
+        else emit("add-to-cart", props.product)
+      }
+
+      watch(slidePosition, () => {
+        currentSlidePosition.value = slidePosition.value
+      })
+
+      return {
+        Logic,
+        viewProduct,
+        defaultBanner,
+        toggleCart,
+        viewProductDetails,
+        showProductDetailsModal,
+        currentSlidePosition,
+        slidePosition,
+        seeMoreDescription,
+        viewMerchantDetails,
+        getBottomPadding,
+      }
     },
-    isInCart: {
-      type: Boolean,
-      default: false,
-    },
-    allowViewMerchantDetails: {
-      type: Boolean,
-      default: true,
-    },
-  },
-  emits: ["view-product", "add-to-cart"],
-  setup(props, { emit }) {
-    const defaultBanner = "/images/greep-transparent-logo.svg";
-    const showProductDetailsModal = ref(false);
-    const seeMoreDescription = ref(false);
-    const currentSlidePosition = ref(0);
-    const slidePosition = ref(0);
-
-    const viewProductDetails = () => {
-      showProductDetailsModal.value = true;
-    };
-
-    const viewProduct = () => {
-      showProductDetailsModal.value = false;
-      emit("view-product", props.product);
-    };
-    const viewMerchantDetails = () => {
-      if (!props.allowViewMerchantDetails) return;
-
-      showProductDetailsModal.value = false;
-      Logic.Common.GoToRoute(`/shops/${props.product.businessUuid}`);
-    };
-    const toggleCart = () => {
-      if (props.isInCart) emit("remove-from-cart", props.product);
-      else emit("add-to-cart", props.product);
-    };
-
-    watch(slidePosition, () => {
-      currentSlidePosition.value = slidePosition.value;
-    });
-
-    return {
-      Logic,
-      viewProduct,
-      defaultBanner,
-      toggleCart,
-      viewProductDetails,
-      showProductDetailsModal,
-      currentSlidePosition,
-      slidePosition,
-      seeMoreDescription,
-      viewMerchantDetails,
-      getBottomPadding,
-    };
-  },
-});
+  })
 </script>
